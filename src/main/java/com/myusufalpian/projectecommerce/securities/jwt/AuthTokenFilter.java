49976ef2be7231651211ee934b59c7e2e510b6a6
myusuf <muhammad.yusuf@difinite.com>
2022-09-07 15:57:24 +0700

    add new file for jwt auth to git

---
+++ src/main/java/com/myusufalpian/projectecommerce/securities/jwt/AuthTokenFilter.java
@@ -0,0 +1,59 @@
+package com.myusufalpian.projectecommerce.securities.jwt;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.myusufalpian.projectecommerce.securities.service.UserDetailsServiceImplementation;
+
+public class AuthTokenFilter extends OncePerRequestFilter{
+    
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private UserDetailsServiceImplementation userDetailsServiceImplementation;
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            String jwt = parseJwt(request); 
+            if(jwt!=null&&jwtUtils.validateJwtToken(jwt)){
+                String username = jwtUtils.getUserNameFromJwtToken(jwt);
+                UserDetails userDetails = userDetailsServiceImplementation.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        } catch (Exception e) {
+            logger.error("Failed: {}", e.getMessage());
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String parseJwt(HttpServletRequest httpServletRequest){
+        String headerAuth = httpServletRequest.getHeader("Authorization");
+        if(StringUtils.hasText(headerAuth)&&headerAuth.startsWith("Bearer")){
+            return headerAuth.substring(7, headerAuth.length());
+        }
+        return null;
+    }
+}
--- src/main/java/com/myusufalpian/projectecommerce/securities/jwt/JwtUtils.java
+++ src/main/java/com/myusufalpian/projectecommerce/securities/jwt/JwtUtils.java
@@ -1,13 +1,20 @@
 package com.myusufalpian.projectecommerce.securities.jwt;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import com.myusufalpian.projectecommerce.securities.service.UserDetailsImplementation;
+
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.SignatureException;
 import io.jsonwebtoken.UnsupportedJwtException;
 
@@ -36,8 +43,17 @@ public class JwtUtils {
         } catch (IllegalArgumentException e) {
             logger.error("Jwt claims string is empty: {}", e.getMessage());    
         }
-
         return false;
+    }
+
+    public String generateJwtToken(Authentication authentication){
+        UserDetailsImplementation principal = (UserDetailsImplementation)authentication.getPrincipal();
+
+        return Jwts.builder().setSubject((principal.getUsername())).setIssuedAt(new Date()).setExpiration(new Date((new Date()).getTime() + jwtExpirationMs)).signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
+    }
+
+    public String getUserNameFromJwtToken(String token){
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
     }
 
 }
