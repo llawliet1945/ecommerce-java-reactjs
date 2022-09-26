e840a615f3373d6cb190c50a8a1345791e713f77
myusuf <muhammad.yusuf@difinite.com>
2022-09-26 14:06:19 +0700

    clean code keranjang repository

--- src/main/java/com/myusufalpian/projectecommerce/models/repositories/KeranjangRepository.java
+++ src/main/java/com/myusufalpian/projectecommerce/models/repositories/KeranjangRepository.java
@@ -6,13 +6,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface KeranjangRepository extends JpaRepository<KeranjangEntity, String> {
 
-    // @Query(nativeQuery = true, value = "select * from tb_keranjang tk where product_id=:produkId and user_username=:username")
     Optional<KeranjangEntity> findByUserUsernameAndProductId(String username, String produkId);
 
-    // @Query(nativeQuery = true, value = "select * from tb_keranjang tk where user_username=:username")
     List<KeranjangEntity> findByUserUsername(String username);
 }
